fba822e3f1df0a87b7b4ce3f79c60b8900085fab
Gustavo Tolay <jeremiastolay1714@gmail.com>
2023-05-06 03:27:36 -0300

    hotfix

--- build/mongoose.js
+++ build/mongoose.js
@@ -72,8 +72,7 @@ const uri = process.env.DATABASE_URI;
 const DbConnect = () => __awaiter(void 0, void 0, void 0, function* () {
     yield (0, mongoose_1.connect)(uri, {
         user: process.env.DATABASE_USER,
-        pass: process.env.DATABASE_PASS,
-        authSource: process.env.DATABASE_AUTH
+        pass: process.env.DATABASE_PASS
     });
     console.log("connected with database");
 });
